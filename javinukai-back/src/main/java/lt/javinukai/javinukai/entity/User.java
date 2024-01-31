d39de82247c324cc96fef733c62673ea82e5461d
rkukutis <kukutis.ricardas@gmail.com>
2024-01-31 12:48:15 +0200

    feat: update user entity, added service and repository

--- javinukai-back/src/main/java/lt/javinukai/javinukai/entity/User.java
+++ javinukai-back/src/main/java/lt/javinukai/javinukai/entity/User.java
@@ -1,6 +1,9 @@
 package lt.javinukai.javinukai.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import java.time.ZonedDateTime;
@@ -8,17 +11,28 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Builder
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID uuid;
+    @Setter
     private String name;
+    @Setter
     private String surname;
+    @Setter
+    private String email;
+    @Setter
     private Integer birthYear;
+    @Setter
     private String phoneNumber;
+    @Setter
     private Boolean isFreelance;
+    @Setter
     private String institution;
+
     @CreatedDate
     private ZonedDateTime createdAt;
     @LastModifiedDate
