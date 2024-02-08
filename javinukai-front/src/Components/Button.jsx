68778fcb3760b80e40890628691b9d4067e8b51d
BenDar77 <darguzasbenas@gmail.com>
2024-02-08 18:49:45 +0200

    Added Input, SearchBar, Table components.

--- javinukai-front/src/Components/Button.jsx
+++ javinukai-front/src/Components/Button.jsx
@@ -1,7 +1,7 @@
 function Button() {
   return (
     <div>
-      <button className="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full">
+      <button className="bg-blue-400 hover:bg-blue-500 text-white py-2 px-4 rounded-full">
         Button
       </button>
     </div>
