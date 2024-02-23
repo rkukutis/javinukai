95a51a78cc038a6349a992049df532ad3e67cf03
Rick K <kukutis.ricardas@gmail.com>
2024-02-23 17:14:14 +0200

    fix: fixed DropDownMenu conflicts, backend issues from merge request

---
+++ javinukai-back/src/test/java/lt/javinukai/javinukai/service/AuthenticationServiceTest.java
@@ -0,0 +1,27 @@
+package lt.javinukai.javinukai.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class AuthenticationServiceTest {
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void confirmEmail() {
+    }
+
+    @Test
+    void forgotPassword() {
+    }
+
+    @Test
+    void resetPassword() {
+    }
+}
