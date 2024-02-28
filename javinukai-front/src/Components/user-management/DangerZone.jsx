f950558696bb200da871e475c52bf4613e14066f
Rick K <kukutis.ricardas@gmail.com>
2024-02-28 12:35:10 +0200

    fix(user-management): made user deletion toast more informative

--- javinukai-front/src/Components/user-management/DangerZone.jsx
+++ javinukai-front/src/Components/user-management/DangerZone.jsx
@@ -7,33 +7,9 @@ import useUserStore from "../../stores/userStore";
 import { useNavigate } from "react-router-dom";
 import deleteUser from "../../services/users/deleteUser";
 
-function ConfirmationModal({ children, onResponse, onModalSet }) {
-  return (
-    <div className="absolute top-0 left-0 w-full h-full backdrop-blur-md backdrop-brightness-50 flex flex-col justify-center items-center">
-      <div className="lg:w-[20rem] bg-white flex flex-col space-y-4 items-center px-6 py-4 rounded-md">
-        <h1 className="text-2xl text-center">{children}</h1>
-        <div className="grid grid-cols-2 w-full gap-x-4">
-          <Button
-            extraStyle="bg-green-500 hover:bg-green-400"
-            onClick={() => onResponse(true)}
-          >
-            Yes
-          </Button>
-          <Button
-            extraStyle="bg-red-500 hover:bg-red-400"
-            onClick={() => onResponse(false)}
-          >
-            No
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
-}
-
 export function DangerZone({ userData }) {
   const { user } = useUserStore((state) => state);
-  const getUserMutation = useMutation({
+  const updateUserMutation = useMutation({
     mutationFn: (data) => updateUser(data),
     onSuccess: () => {
       toast.success("User permissions changed");
@@ -43,7 +19,9 @@ export function DangerZone({ userData }) {
   const deleteUserMutation = useMutation({
     mutationFn: (data) => deleteUser(data),
     onSuccess: () => {
-      toast.success(`User ${userData.uuid} deleted successfully`);
+      toast.success(
+        `User ${userData.name} ${userData.surname} (${userData.email}) deleted successfully`
+      );
       queryClient.invalidateQueries(["users"]);
       navigate("/manage-users");
     },
@@ -54,7 +32,7 @@ export function DangerZone({ userData }) {
 
   function handleChangeRole() {
     if (newRole == userData.role) {
-      toast.error("New role is the same as old one");
+      toast.error("New role is the same as current one");
       return;
     }
     if (
@@ -63,7 +41,7 @@ export function DangerZone({ userData }) {
       )
     )
       return;
-    getUserMutation.mutate({ ...userData, role: newRole });
+    updateUserMutation.mutate({ ...userData, role: newRole });
   }
 
   function handleToggleLockAccount() {
@@ -75,7 +53,10 @@ export function DangerZone({ userData }) {
       )
     )
       return;
-    getUserMutation.mutate({ ...userData, isNonLocked: !userData.isNonLocked });
+    updateUserMutation.mutate({
+      ...userData,
+      isNonLocked: !userData.isNonLocked,
+    });
   }
 
   function handleDeleteAccount() {
