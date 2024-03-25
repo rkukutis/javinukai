327da981f8ab8c5778d5db6569f910b7639ef719
BenDar77 <darguzasbenas@gmail.com>
2024-03-25 16:05:34 +0200

    BackButton translated

--- javinukai-front/src/Components/BackButton.jsx
+++ javinukai-front/src/Components/BackButton.jsx
@@ -1,12 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
+import { useTranslation } from "react-i18next";
 
 function BackButton({ extraStyle }) {
+  const { t } = useTranslation();
   const navigate = useNavigate();
 
   return (
     <Button extraStyle={extraStyle + " mx-1"} onClick={() => navigate(-1)}>
-      Back
+      {t("UserDetailsPage.backButton")}
     </Button>
   );
 }
