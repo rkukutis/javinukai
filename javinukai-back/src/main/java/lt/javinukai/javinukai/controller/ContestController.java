be1e7fcaace36631bb7555b970977fb9469ae585
rkukutis <kukutis.ricardas@gmail.com>
2024-03-25 14:00:23 +0200

    fix: added missing bracket

--- javinukai-back/src/main/java/lt/javinukai/javinukai/controller/ContestController.java
+++ javinukai-back/src/main/java/lt/javinukai/javinukai/controller/ContestController.java
@@ -121,15 +121,15 @@ public class ContestController {
 
     @DeleteMapping(path = "/contests/{id}/delete")
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    public ResponseEntity<?> deleteContest(@PathVariable @NotNull UUID id) {
+    public ResponseEntity<String> deleteContest(@PathVariable @NotNull UUID id) {
         log.info("Request for deleting contest with ID: {}", id);
         contestService.deleteContest(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    
+    }
 
     @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_MODERATOR')")
     @PatchMapping(path = "/contests/{contestId}/reset")
-    public ResponseEntity<?> startNewStage (@PathVariable UUID contestId){
+    public ResponseEntity<String> startNewStage (@PathVariable UUID contestId){
         contestService.startNewCompetitionStage(contestId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
